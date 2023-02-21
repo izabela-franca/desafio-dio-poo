52b91223c66fbd5b8c2b30e7d1b90c791c4809ce
Izabela França <ifranca.sc@gmail.com>
2023-02-21 07:40:12 -0300

    Criando classes: Mentoria e Curso

---
+++ src/desafio/Main.java
@@ -0,0 +1,30 @@
+package desafio;
+
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Aprenda o básico sobre Java.");
+        curso1.setCargaHoraria(15);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso C#");
+        curso2.setDescricao("Aprenda o básico sobre C#.");
+        curso2.setCargaHoraria(15);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Crie seu portfolio");
+        mentoria1.setDescricao("Aprenda a criar seu portfólio para iniciar sua carreira como Dev.");
+        mentoria1.setData(LocalDate.now());
+        System.out.println(mentoria1);
+    }
+}
